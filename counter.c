2b3601d6158fa36ad73b5c47e1fe64acfaadae24
Suchit <suchitkarunakaran@gmail.com>
2024-12-14 12:25:44 +0530

    Count IP packets by IPv4 address

--- counter.c
+++ counter.c
@@ -1,21 +1,38 @@
 //go:build ignore
 
 #include <linux/bpf.h>
-#include <bpf/bpf_helpers.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include "headers/bpf_helpers.h"
 
 struct {
-    __uint(type, BPF_MAP_TYPE_ARRAY);
-    __type(key, __u32);
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __type(key, __be32);
     __type(value, __u64);
-    __uint(max_entries, 1);
+    __uint(max_entries, 1024);
 } pkt_count SEC(".maps");
 
 SEC("xdp")
-int count_packets() {
-    __u32 key = 0;
-    __u64 *count = bpf_map_lookup_elem(&pkt_count, &key);
+int xdp_ip_packet_counter(struct xdp_md* ctx) {
+    void *data = (void*)(long)ctx->data;
+    void *data_end = (void*)(long)ctx->data_end;
+    struct ethhdr *eth = data;
+    if ((void*)(eth + 1) > data_end) {
+        return XDP_PASS;
+    }
+    if (eth->h_proto != __constant_htons(ETH_P_IP)) {
+        return XDP_PASS;
+    }
+    struct iphdr *ip = (struct iphdr*)(eth + 1);
+    if ((void*)(ip + 1) > data_end) {
+        return XDP_PASS;
+    }
+    __u64 *count, init_val = 1;
+    count = bpf_map_lookup_elem(&pkt_count, &ip->saddr);
     if (count) {
         __sync_fetch_and_add(count, 1);
+    } else {
+        bpf_map_update_elem(&pkt_count, &ip->saddr, &init_val, BPF_ANY);
     }
     return XDP_PASS;
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"log"
 	"net"
@@ -8,9 +9,18 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
-    "github.com/cilium/ebpf/rlimit"
+	"github.com/cilium/ebpf/rlimit"
 )
+
+const bpfProgramPath = "counter.o"
+
+func ipToString(ip uint32) net.IP {
+	address := make(net.IP, 4)
+	binary.LittleEndian.PutUint32(address, ip)
+	return address
+}
 
 func main() {
 	if len(os.Args) < 3 || os.Args[1] != "-count" {
@@ -20,39 +30,55 @@ func main() {
 	if err := rlimit.RemoveMemlock(); err != nil {
 		log.Fatal("Error while removing memlock:", err)
 	}
-	var objs counterObjects
-	if err := loadCounterObjects(&objs, nil); err != nil {
-		log.Fatal("Error while loading eBPF objects:", err)
+	spec, err := ebpf.LoadCollectionSpec(bpfProgramPath)
+	if err != nil {
+		log.Fatal("Failed to load the BPF program:", err)
 	}
-	defer objs.Close()
+	collection, err := ebpf.NewCollection(spec)
+	if err != nil {
+		log.Fatal("Failed to create BPF collection:", err)
+	}
+	defer collection.Close()
+	ipMap := collection.Maps["pkt_count"]
+	if ipMap == nil {
+		log.Fatal("Failed to find pkt_count map in the BPF program")
+	}
 	ifname := os.Args[2]
 	iface, err := net.InterfaceByName(ifname)
 	if err != nil {
 		log.Fatalf("Error while getting interface %s: %s", ifname, err)
 	}
+	prog := collection.Programs["xdp_ip_packet_counter"]
+	if prog == nil {
+		log.Fatal("Failed to find xdp_ip_packet_counter in the BPF program")
+	}
 	link, err := link.AttachXDP(link.XDPOptions{
-		Program: objs.CountPackets,
+		Program:   prog,
 		Interface: iface.Index,
 	})
 	if err != nil {
-		log.Fatal("Error while attaching function:", err)
+		log.Fatal("Failed to attach XDP program:", err)
 	}
 	defer link.Close()
-	log.Printf("Counting incoming packets on %s...", ifname)
+	fmt.Printf("eBPF program attached to %s\n", iface.Name)
 	tick := time.Tick(time.Second)
 	stop := make(chan os.Signal, 5)
 	signal.Notify(stop, os.Interrupt)
+	fmt.Println("Counting packets by IP address.")
 	for {
 		select {
 		case <-tick:
+			iter := ipMap.Iterate()
+			var ip uint32
 			var count uint64
-			err := objs.PktCount.Lookup(uint32(0), &count)
-			if err != nil {
-				log.Fatal("Map lookup:", err)
+			for iter.Next(&ip, &count) {
+				log.Printf("%s: %d packets\n", ipToString(ip), count)
 			}
-			log.Printf("Received %d packets", count)
+			if err := iter.Err(); err != nil {
+				log.Fatal("Error iterating map:", err)
+			}
 		case <-stop:
-			log.Print("Received signal, exiting...")
+			log.Println("Received signal, exiting...")
 			return
 		}
 	}
